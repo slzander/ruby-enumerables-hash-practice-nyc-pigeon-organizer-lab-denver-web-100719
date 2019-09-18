279d70b8a82a1d7d2a2e3483361fd08c2f8349d7
Stacey Zander <staceyzander@gmail.com>
2019-09-18 03:24:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |trait, attribute|
-    attribute.each do |
-      
+    attribute.each do |att, names|
+      names
     end 
   end 
 end
